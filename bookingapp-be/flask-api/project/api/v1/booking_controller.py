5a7c8f9e84508cda39cd3f019953dcc11d91a877
ChuongNT99 <chuongnt_tts@rikkeisoft.com>
2023-12-01 15:41:12 +0700

    WIBA-113 api view calendar

--- bookingapp-be/flask-api/project/api/v1/booking_controller.py
+++ bookingapp-be/flask-api/project/api/v1/booking_controller.py
@@ -14,7 +14,6 @@ from datetime import datetime, timedelta
 from typing import Dict
 
 
-
 booking_blueprint = Blueprint('booking_controller', __name__)
 
 
@@ -29,4 +28,61 @@ def get_bookings() -> dict:
     except BadRequest as e:
         return BaseResponse.error(e)
     except InternalServerError as e:
+        return BaseResponse.error(e)
+
+@booking_blueprint.route("/bookings", methods=["POST"])
+@jwt_required()
+@has_permission("create")
+def book_room_endpoint() -> dict:
+    try:
+        data = request.get_json()
+        response_data: dict = BookingService.book_room(data)
+
+        return response_data
+
+    except BadRequest as e:
+        return BaseResponse.error(e)
+
+    except Conflict as e:
+        return BaseResponse.error(e)
+
+    except InternalServerError as e:
+        return BaseResponse.error(e)
+
+@booking_blueprint.route("/bookings/<int:booking_id>", methods=["PUT"])
+@jwt_required()
+@has_permission("update")
+def update_booking_endpoint(booking_id: int):
+    try:
+        data = request.get_json()
+        response_data = BookingService.update_booking(booking_id, data)
+        return response_data
+
+    except BadRequest as e:
+        return BaseResponse.error(e)
+
+    except Conflict as e:
+        return BaseResponse.error(e)
+
+    except NotFound as e:
+        return BaseResponse.error(e)
+
+    except InternalServerError as e:
+        return BaseResponse.error(e)
+
+@booking_blueprint.route("/bookings/<int:booking_id>", methods=["DELETE"])
+@jwt_required()
+@has_permission("delete")
+def delete_booking(booking_id: int) -> Dict:
+    try:
+        response_data = BookingService.delete_booking_service(booking_id)
+        return response_data
+    except NotFound as e:
+        return BaseResponse.error(e)
+
+    except BadRequest as e:
+        return BaseResponse.error(e)
+
+    except IntegrityError:
+        db.session.rollback()
         return BaseResponse.error(e)
--- bookingapp-be/flask-api/project/database/excute/booking.py
+++ bookingapp-be/flask-api/project/database/excute/booking.py
@@ -1,12 +1,76 @@
 from project.models import Booking, BookingUser, Room
 from typing import List, Optional, Union
 from project import db
-
+from flask_jwt_extended import get_jwt_identity
 
 class BookingExecutor:
+    @staticmethod
+    def get_booking(booking_id: int) -> Optional[Booking]:
+        return Booking.query.get(booking_id)
+    
     @staticmethod
     def get_bookings_in_date_range(start_date, end_date) -> List[Booking]:
         return Booking.query.filter(
             Booking.is_deleted == False,
             Booking.time_end.between(start_date, end_date)
         ).all()
+
+    @staticmethod
+    def check_room_availability(room_id: int, time_start: str, time_end: str) -> Optional[Booking]:
+        return Booking.query.filter(
+            Booking.room_id == room_id,
+            Booking.time_end >= time_start,
+            Booking.time_start <= time_end
+        ).first()
+        
+    @staticmethod
+    def create_booking(room_id: int, title: str, time_start: str, time_end: str, user_ids: List[int]) -> Booking:
+        try:
+            new_booking = Booking(
+                room_id=room_id, title=title, time_start=time_start, time_end=time_end, is_accepted=True, is_deleted=False)
+            db.session.add(new_booking)
+            db.session.commit()
+
+            for user_id in user_ids:
+                user_booking = BookingUser(
+                    user_id=user_id, booking_id=new_booking.booking_id, is_attending=False)
+                db.session.add(user_booking)
+            db.session.commit()
+
+            return new_booking
+        except Exception as e:
+            db.session.rollback()
+            raise e
+        
+    @staticmethod
+    def check_room_availability_update(room_id: int, time_start: str, time_end: str, booking_id: int) -> Optional[Booking]:
+        return Booking.query.filter(
+            Booking.room_id == room_id,
+            Booking.time_end >= time_start,
+            Booking.time_start <= time_end,
+            Booking.booking_id != booking_id
+        ).first()
+        
+    @staticmethod
+    def update_booking(booking: Booking, room_id: int, title: str, time_start: str, time_end: str, user_ids: List[int]) -> None:
+        try:
+            booking.room_id = room_id
+            booking.title = title
+            booking.time_start = time_start
+            booking.time_end = time_end
+
+            BookingUser.query.filter_by(booking_id=booking.booking_id).delete()
+
+            for user_id in user_ids:
+                user_booking = BookingUser(
+                    user_id=user_id, booking_id=booking.booking_id, is_attending=False)
+                db.session.add(user_booking)
+
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            raise e
+        
+    @staticmethod
+    def is_room_blocked(room_id: int) -> Union[bool, None]:
+        return Room.query.filter_by(room_id=room_id).value(Room.is_blocked)
