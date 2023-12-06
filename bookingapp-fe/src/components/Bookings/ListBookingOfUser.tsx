5cec87cdde8ab769937ca6fb2dd8f425f5b24c14
lhtvi29 <vilht_tts@rikkeisoft.com>
2023-12-06 09:31:28 +0700

    update constant

--- bookingapp-fe/src/components/Bookings/ListBookingOfUser.tsx
+++ bookingapp-fe/src/components/Bookings/ListBookingOfUser.tsx
@@ -1,110 +1,145 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { url } from '../ultils/urlApi';
-import { BookingData, token } from '../constant/constant';
-import { Card, Descriptions, List, Tag } from 'antd';
-import moment from 'moment';
-import "./Booking.css"
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { url } from "../ultils/urlApi";
+import {
+  BookingData,
+  formatDate,
+  formatTime,
+  statuTag,
+  token,
+} from "../constant/constant";
+import { Card, Descriptions, List } from "antd";
+import "./Booking.css";
+import { handleError } from "../ultils/ultilsApi";
+import { showPopup } from "../ultils/Popup";
 
 const ListBookingOfUser = () => {
-    const [listBooking, setListBooking] = useState<BookingData[]>([])
-    const [perPage, setPerPage] = useState(1);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalItems, setTotalItems] = useState(0);
-    const [loading, setLoading] = useState<boolean>(false);
-    const getData = async () => {
-        setLoading(true);
-        try {
-            await axios
-                .get(url + "/v1/user/view_booked", {
-                    params: {
-                        page: currentPage,
-                        per_page: perPage,
-                    },
-                    withCredentials: true,
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                })
-                .then((response) => {
-                    setListBooking(response.data.data.list_bookings);
-                    setTotalItems(response.data.data.total_items);
-                    setPerPage(response.data.data.per_page);
-                });
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setLoading(false);
-        }
-    };
-    useEffect(() => {
-        getData();
-    }, [currentPage, perPage]);
-    const handlePageChange = (page: number) => {
-        setCurrentPage(page);
-        console.log(currentPage)
-    };
-    const handlePageSizeChange = (pageSize: number) => {
-        const newPerPage = pageSize;
-        const newCurrentPage =
-            Math.ceil(((currentPage - 1) * perPage) / newPerPage) + 1;
-        setCurrentPage(newCurrentPage);
-        console.log(newCurrentPage)
-    };
-    const pagination = {
-        current: currentPage,
-        pageSize: perPage,
-        total: totalItems,
-        onChange: handlePageChange,
-        onShowSizeChange: handlePageSizeChange,
-    };
-const customLabelStyle = {
-        fontWeight: 'bold',
-        marginRight: '8px',
-        color: 'black'
-    };
-    const customContentStyle = {
-        display: 'flex',
-        justifyContent: 'end',
-        marginRight: 100
-    };
+  const [listBooking, setListBooking] = useState<BookingData[]>([]);
+  const [perPage, setPerPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalItems, setTotalItems] = useState(0);
+  const [loading, setLoading] = useState<boolean>(false);
+  useEffect(() => {
+    getData();
+  }, [currentPage, perPage]);
+  const getData = async () => {
+    setLoading(true);
+    try {
+      await axios
+        .get(url + "/v1/user/view_booked", {
+          params: {
+            page: currentPage,
+            per_page: perPage,
+          },
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          setListBooking(response?.data?.data?.list_bookings);
+          setTotalItems(response?.data?.data?.total_items);
+          setPerPage(response?.data?.data?.per_page);
+        });
+    } catch (error: any) {
+      const { message, errors }: any = handleError(error);
+      const messageErrors = message + " " + errors;
+      showPopup(false, messageErrors);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+  const handlePageSizeChange = (pageSize: number) => {
+    const newPerPage = pageSize;
+    const newCurrentPage =
+      Math.ceil(((currentPage - 1) * perPage) / newPerPage) + 1;
+    setCurrentPage(newCurrentPage);
+  };
+  const pagination = {
+    current: currentPage,
+    pageSize: perPage,
+    total: totalItems,
+    onChange: handlePageChange,
+    onShowSizeChange: handlePageSizeChange,
+  };
+  const customLabelStyle = {
+    fontWeight: "bold",
+    marginRight: "8px",
+    color: "black",
+  };
+  const customContentStyle = {
+    display: "flex",
+    justifyContent: "end",
+    marginRight: 100,
+  };
 
-    return (
-        <div>
-            <h1 className='component-name'>List of scheduled meetings</h1>
-            <List
-                dataSource={listBooking}
-                pagination={pagination}
-                renderItem={(item: BookingData) => (
-                    <List.Item>
-                        <Card
-                            className='item-booked'
-                            key={item.title}
-                            title={<div className='title-booked'>{item.title}</div>}
-                        >
-                            <div className='info-booked'>
-                                <Descriptions className='detail-booked' layout="horizontal" column={2} >
-                                    <Descriptions.Item labelStyle={customLabelStyle} contentStyle={customContentStyle} label="ROOM">{item.room_name} </Descriptions.Item>
-                                    <Descriptions.Item labelStyle={customLabelStyle} contentStyle={customContentStyle} label="DATE"> {moment(item.time_start).format('DD/MM/YYYY')} </Descriptions.Item>
-                                    <Descriptions.Item labelStyle={customLabelStyle} contentStyle={customContentStyle} label="TOTAL PARTICIPATIONS">{(item.user_name)} </Descriptions.Item>
-                                    <Descriptions.Item labelStyle={customLabelStyle} contentStyle={customContentStyle} label="TIME START"> {moment(item.time_start).format('HH:mm')} </Descriptions.Item>
-                                    <Descriptions.Item>  </Descriptions.Item>
-                                    <Descriptions.Item labelStyle={customLabelStyle} contentStyle={customContentStyle} label="TIME END"> {moment(item.time_end).format('HH:mm')} </Descriptions.Item>
-                                </Descriptions>
-                               <div className='status-booked' >
-                               {(item.is_deleted) ? (<Tag className='status-tag' color='#ff0000'>Rejected</Tag>) : (
-                                    (item.status) ? (<Tag className='status-tag' color='#009900'>Successed</Tag>) : (
-                                        <Tag className='status-tag' color='#ff9933'>Pending</Tag>
-                                    )
-                                )}
-                               </div>
-                            </div>
-                        </Card>
-                    </List.Item>
-                )}
-            />
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1 className="component-name">List of scheduled meetings</h1>
+      <List
+        dataSource={listBooking}
+        pagination={pagination}
+        renderItem={(item: BookingData) => (
+          <List.Item>
+            <Card
+              className="item-booked"
+              key={item.title}
+              title={<div className="title-booked">{item.title}</div>}
+            >
+              <div className="info-booked">
+                <Descriptions
+                  className="detail-booked"
+                  layout="horizontal"
+                  column={2}
+                >
+                  <Descriptions.Item
+                    labelStyle={customLabelStyle}
+                    contentStyle={customContentStyle}
+                    label="ROOM"
+                  >
+                    {item.room_name}
+                  </Descriptions.Item>
+                  <Descriptions.Item
+                    labelStyle={customLabelStyle}
+                    contentStyle={customContentStyle}
+                    label="DATE"
+                  >
+                    {formatDate(item.time_start)}
+                  </Descriptions.Item>
+                  <Descriptions.Item
+                    labelStyle={customLabelStyle}
+                    contentStyle={customContentStyle}
+                    label="TOTAL PARTICIPATIONS"
+                  >
+                    {item.user_name}
+                  </Descriptions.Item>
+                  <Descriptions.Item
+                    labelStyle={customLabelStyle}
+                    contentStyle={customContentStyle}
+                    label="TIME START"
+                  >
+                    {formatTime(item.time_start)}
+                  </Descriptions.Item>
+                  <Descriptions.Item> </Descriptions.Item>
+                  <Descriptions.Item
+                    labelStyle={customLabelStyle}
+                    contentStyle={customContentStyle}
+                    label="TIME END"
+                  >
+                    {formatTime(item.time_end)}
+                  </Descriptions.Item>
+                </Descriptions>
+                <div className="status-booked">{statuTag(item)}</div>
+              </div>
+            </Card>
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+};
 
-export default ListBookingOfUser
+export default ListBookingOfUser;
--- bookingapp-fe/src/constant/constant.js
+++ bookingapp-fe/src/constant/constant.js
@@ -1,10 +1,49 @@
-export const TYPE_USER = { ADMIN : 'admin'}
+export const TYPE_USER = { ADMIN: "admin" };
 export interface DataType {
-    user_id: number;
-    role_id: number[];
-    user_name: string;
-    role_name: string[];
-    phone_number: string;
-    email: string;
-    is_deleted: boolean;
+  user_id: number;
+  role_id: number[];
+  user_name: string;
+  role_name: string[];
+  phone_number: string;
+  email: string;
+  is_deleted: boolean;
+}
+
+export interface BookingData {
+  room_name: string;
+  title: string;
+  user_name: string[];
+  time_start: string;
+  time_end: string;
+  is_eccept: boolean;
+  is_deleted: boolean;
+}
+export const token = getCookie("token");
+
+export const formatTime = (time: any) => {
+  return moment(time).format("HH:mm");
+};
+export const formatDate = (time: any) => {
+  return moment(time).format("DD:MM:YYYY");
+};
+export const statuTag = (item: BookingData) => {
+  if (item.is_deleted) {
+    return (
+      <Tag className="status-tag" color="#ff0000">
+        Rejected
+      </Tag>
+    );
+  } else if (item.is_eccept) {
+    return (
+      <Tag className="status-tag" color="#009900">
+        Successed
+      </Tag>
+    );
+  } else {
+    return (
+      <Tag className="status-tag" color="#ff9933">
+        Pending
+      </Tag>
+    );
   }
+};
