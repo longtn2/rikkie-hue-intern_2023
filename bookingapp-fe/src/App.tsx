b34b042d9b2ab2686d2c59f3993db3503022f763
TienDat2727 <datndpt_tts@rikkeisoft.com>
2023-12-12 17:44:19 +0700

    fix conflict

--- bookingapp-fe/src/App.tsx
+++ bookingapp-fe/src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FormLogin from './components/Form';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './Route/ProtectedRoute';
+import PrivateRoute from './Route/PrivateRoute';
+import CalendarBooking from './components/Bookings/Bookings';
+import UsersManager from './components/UserMananger/UserManager';
+import Dashboard from './components/dashboard/dashboard';
+import RoomDetails from './components/room/RoomDetails';
+import LayoutApp from './components/Layout/Layout';
+import InfoUser from './InfoAccount/InfoUser';
+import ListBookingOfUser from './components/Booking/ListBookingOfUser';
+import WaitingBookingList from './components/Booking/WaitingBookingList';
+import InvitationList from './components/Booking/InvitationList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route path='/login' element={<FormLogin />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path='/' element={<LayoutApp />}>
+            <Route path='/calendarmeeting' element={<CalendarBooking />} />
+            <Route path='/informationaccount' element={<InfoUser/>}/>
+            <Route path='/bookingroom' element={<ListBookingOfUser/>}/>
+            <Route path='/invitations' element = {<InvitationList/>}/>
+            <Route element={<PrivateRoute />}>
+              <Route path='/roomManager' element={<Rooms />} />
+              <Route path='/roomManager/:id' element={<RoomDetails />} />
+              <Route path='/usermanager' element={<UsersManager />} />
+              <Route path='/bookingmanager' element={<WaitingBookingList/>}/>
+              <Route path='/' element={<Dashboard />} />
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
